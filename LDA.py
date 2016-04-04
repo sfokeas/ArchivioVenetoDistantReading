6f2a2974f65c17295220b8e6618269bfbce53736
vidit09 <vidit7@outlook.com>
2016-04-04 17:04:20 +0200

    Update the code for new data

--- LDA.py
+++ LDA.py
@@ -6,16 +6,20 @@ from nltk.corpus import stopwords
 from nltk.stem.snowball import ItalianStemmer
 from nltk.tokenize import TreebankWordTokenizer
 
+filename = "1000articles"
 excludeSet = string.punctuation + ""
 totalNumberOfWords = 8255414
 totalNumberOfDocuments = 98
 
 stemmer = ItalianStemmer()
-
+lineCounter = 0;
 
 def preprocessLine(strLine):
+    global lineCounter
+    lineCounter = lineCounter + 1
+    print lineCounter
     strLine = strLine.decode('utf8').lower()
-    strLine = ''.join(stemmer.stem(ch) for ch in strLine if ch not in set(excludeSet)) #todo put a print just to now the progress of the algo
+    strLine = ''.join(ch for ch in strLine if ch not in set(excludeSet)) #todo put a print just to now the progress of the algo
     return strLine
 
 
@@ -23,24 +27,25 @@ def preprocessLine(strLine):
 
 # Why split sentences???
 
-
+print "Start"
 # Build dictionary
 stopWords = set(stopwords.words('italian'))
 tokenizer = TreebankWordTokenizer()
 dictionary = corpora.Dictionary(
     tokenizer.tokenize(
         preprocessLine(line)
-    ) for line in open('one_journal_per_line'))
+    ) for line in open(filename))
 
 # statistics: how many words in the dictionary. These are the number of difference words in the documents.
 # statistics: wc * to find out the number of words,lines,characters inside journal.
 
 totalNumberOfTokens = len(dictionary.keys())  #save pure dictionary!
+print "Number of Tokens:" + str(totalNumberOfTokens) 
 
-dictionary.filter_extremes(no_below=totalNumberOfDocuments * 0.01, no_above=0.8, keep_n=None);
+dictionary.filter_extremes(no_above=0.3, keep_n=None);
 # keep tokens that appear in more that 1% of documents
 # keep tokens that don't appear in more than 80% of documents
-
+print "Dictionary Created"
 
 # remove stop words and rare words
 stop_ids = [dictionary.token2id[stopword] for stopword in stopWords
@@ -49,12 +54,15 @@ stop_ids = [dictionary.token2id[stopword] for stopword in stopWords
 # filter words that do not appear frequently
 rare = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
 
-oneLetterWords = [tokenid for tokenid in dictionary.keys() if len(dictionary.get(tokenid)) == 1] # doesn't work. goal was to remove one leter words
-
+oneLetterWords = [tokenid for tokenid in dictionary.keys() if len(dictionary.get(tokenid)) == 1] 
 dictionary.filter_tokens(stop_ids + rare + oneLetterWords)
 dictionary.compactify()  # remove gaps in id sequence after words that were removed
 
 numTokensAfterExtremeRemoval = len(dictionary.keys())
+print "Number of TokensAfterExtremeRemoval:" + str(numTokensAfterExtremeRemoval) 
+
+
+
 
 dictionary.save('strReviews.dict')
 
@@ -62,11 +70,11 @@ dictionary = corpora.Dictionary.load('strReviews.dict')
 
 
 # build corpus (vector representation of texts)
-
+print "Preprocess Done"
 
 class MyCorpus(object):
     def __iter__(self):
-        for line in open('one_journal_per_line'):
+        for line in open(filename):
             # assume there's one document per line, tokens separated by whitespace
             yield dictionary.doc2bow(tokenizer.tokenize(preprocessLine(line)))
 
@@ -75,14 +83,14 @@ corpus = MyCorpus()
 
 # build LDA
 
-lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=20) # model = hdpmodel.HdpModel(bow_corpus, id2word=dictionary) # tfidf = models.TfidfModel(corpus); corpus_tfidf = tfidf[corpus]; lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=4) # model = hdpmodel.HdpModel(bow_corpus, id2word=dictionary) # tfidf = models.TfidfModel(corpus); corpus_tfidf = tfidf[corpus]; lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
 
 # final output is topics... topics are coefficients over a set of words...
 
 lda.save('lda_journals.model')  # <<---- don't forget to execute this on the console!
 
 # get topics for each document
-for line in open('one_journal_per_line'):
+for line in open(filename):
     print lda.get_document_topics(dictionary.doc2bow(tokenizer.tokenize(preprocessLine(line))))
 
 # lda2 = ldamodel.LdaModel.load
@@ -96,7 +104,7 @@ for line in open('one_journal_per_line'):
 
 
 topics = open('topic.txt', 'w')
-out = lda.print_topics(20, 10)
+out = lda.print_topics(4, 20)
 for ind in out:
     for ind2 in ind:
         if isinstance(ind2, (int)):
